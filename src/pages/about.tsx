e5efa5f60ad52e442c95541c031fff6fb1fa781c
Sadra <sadra.shameli1@gmail.com>
2023-04-12 17:04:24 +0200

    Updated logo, favicon

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -22,7 +22,7 @@ export default function About() {
 
                     <div className='flex flex-col items-end'>
                         <div className='grid place-items-center'>
-                            <Image width={500} height={500} src={'/img/logo.jpg'} quality={100} alt='Logo' />
+                            <Image width={500} height={500} src={'/img/logo.png'} quality={100} alt='Logo' />
                             <p className='pt-4'>{SiteInto.logoDescription}</p>
                         </div>
                     </div>
